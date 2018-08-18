0f9627a1f4e5851b2d9877466895615c20952019
andrea.m.carver@gmail.com <andrea.m.carver@gmail.com>
2018-08-18 08:28:30 -0500

    Need to clean up components but otherwise game should work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -34,41 +34,11 @@ class App extends Component {
     }
   };
 
-  handleIncrement = () => {
-    const currentScore = this.state.currentScore + 1;
-    this.setState({
-      currentScore: currentScore,
-      isCorrect: ""
-    });
-    if (currentScore > this.state.topScore) {
-      this.setState({ topScore: currentScore });
-    } else if (currentScore === 12) {
-      this.setState({ isCorrect: "VERY NICE" });
-    }
-    //adds one to current score
-  };
-
-  // handleReset = () => {
-  //   //resets score
-  //   this.setState({
-  //     currentScore: 0,
-  //     topScore: this.state.topScore,
-  //     isCorrect: "Better Luck Next Time!",
-  //     clicked: []
-  //   });
-  //   this.handleShuffle();
-  // };
-
-  // handleShuffle = () => {
-  //   let shuffledCards = shuffleCards(img);
-  //   this.setState({ img: shuffledCards });
-  //   //shuffles the cards
-  // };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">React Clicky Game</h1>
+          <h1 className="App-title">Fishy Clicky Game</h1>
         </header>
         <div className="Game-header">
           <ul>
@@ -81,38 +51,22 @@ class App extends Component {
           </ul>
         </div>
         <div className="App-intro" onClick={this.handleClick}>
-          <img src={img[0]} id="0" />
-          <img src={img[1]} id="1" />
-          <img src={img[2]} id="2" />
-          <img src={img[3]} id="3" />
-          <img src={img[4]} id="4" />
-          <img src={img[5]} id="5" />
-          <img src={img[6]} id="6" />
-          <img src={img[7]} id="7" />
-          <img src={img[8]} id="8" />
-          <img src={img[9]} id="9" />
-          <img src={img[10]} id="10" />
-          <img src={img[11]} id="11" />
+          <img src={img[0]} />
+          <img src={img[1]} />
+          <img src={img[2]} />
+          <img src={img[3]} />
+          <img src={img[4]} />
+          <img src={img[5]} />
+          <img src={img[6]} />
+          <img src={img[7]} />
+          <img src={img[8]} />
+          <img src={img[9]} />
+          <img src={img[10]} />
+          <img src={img[11]} />
         </div>
       </div>
     );
   }
 }
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
